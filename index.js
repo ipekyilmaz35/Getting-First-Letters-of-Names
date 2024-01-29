289d5fb7ec81b7b20805dca8021f243a0a9a53eb
ipekyilmaz35 <ipekyi35@gmail.com>
2024-01-29 16:22:28 +0100

    change for the forEach() method

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 const person = ['Harry', 'Emma', 'Lily', 'Logan', 'Oliver' ];
-
-let firstLetter = [];
-
-for (let i=0; i<person.length; i++){
-  firstLetter.push(person[i][0]);
-}
-console.log(firstLetter);
+person.forEach((element) => console.log(element[0]));
